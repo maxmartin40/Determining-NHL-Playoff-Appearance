1ee172928ed0a3f08241fd7ea083549f19701f69
MaxMartin <maxmartin40@yahoo.com>
2026-04-16 12:57:22 -0600

    Create first visual for Minnesota Wild statistics

--- exploratory_visuals.py
+++ exploratory_visuals.py
@@ -1,4 +1,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 data = pd.read_csv("teams_scaled.csv")
+
+min_data = data[data['team']=="MIN"]
+#print(min_data.head())
+
+plt.bar(min_data['season'], min_data['xGoalsFor_PerGame'], color='green')
+plt.xlabel("Season")
+plt.ylabel("corsiPercentage")
+plt.xticks(np.arange(2008, 2026, step=1))
+plt.tick_params("x", rotation=90)
+# plt.title("Bar Chart Example")
+plt.show()
