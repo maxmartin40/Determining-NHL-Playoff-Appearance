0a137f7e26590f9f824ca80fa09436bcc2934386
MaxMartin <maxmartin40@yahoo.com>
2026-04-16 18:27:46 -0600

    Create legend, color bar chat by whether the team made the playoffs

--- exploratory_visuals.py
+++ exploratory_visuals.py
@@ -1,16 +1,22 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.patches as mpatches
 
 data = pd.read_csv("teams_scaled.csv")
 
 min_data = data[data['team']=="MIN"]
 #print(min_data.head())
 
-plt.bar(min_data['season'], min_data['xGoalsFor_PerGame'], color='green')
+red_patch = mpatches.Patch(color='red', label='Missed Playoffs')
+green_patch = mpatches.Patch(color='green', label='Made Playoffs')
+colors = ['green' if x == 1 else 'red' for x in min_data['playoff']]
+
+plt.bar(min_data['season'], min_data['xReboundsFor_PerGame'], label='playoff', color=colors)
 plt.xlabel("Season")
 plt.ylabel("corsiPercentage")
 plt.xticks(np.arange(2008, 2026, step=1))
 plt.tick_params("x", rotation=90)
-# plt.title("Bar Chart Example")
+plt.legend(handles=[red_patch, green_patch])
+#plt.title("Bar Chart Example")
 plt.show()
